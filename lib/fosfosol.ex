5e3b50af35525678940806583f5f706fc5ff12b5
Bruno Parga <brunoparga@gmail.com>
2024-02-22 23:55:16 +0200

    It works pretty nice. It is just an ungodly mess.

--- lib/fosfosol.ex
+++ lib/fosfosol.ex
@@ -15,12 +15,33 @@ defmodule Fosfosol do
   Run the synchronization between Google Spreadsheets and Anki.
   """
   def sync do
+    case File.rm("./report") do
+      :ok -> :ok
+      {:error, _reason} -> :ok
+    end
+
+    file = File.open!("./report", [:append, :utf8])
+    write = build_writer(file)
+
     {:ok, sheet} =
       Sheet.Supervisor.spreadsheet(Application.fetch_env!(:fosfosol, :file_id))
 
-    {:ok, anki_ids} = AnkiConnect.find_notes(%{query: "deck:M-224"})
+    sheet_rows = read_sheet_rows(sheet)
+    # first we figure out which words are in the spreadsheet but not Anki
+    anki_needs_cards =
+      Enum.reject(sheet_rows, fn
+        # [row_number, front, back, id]
+        # if there is no ID, the length of the row is 3
+        row -> length(row) == 4
+      end)
 
-    sheet_rows = read_sheet_ids(sheet)
+    write.("Words in Sheets but not Anki:")
+    write.(anki_needs_cards)
+
+    # Then we figure out which rows in the spreadsheet have flashcards
+    # but the corresponding ID is not in the sheet. We do that by excluding
+    # the IDs *present* in the sheet from the list of Anki IDs.
+    {:ok, anki_ids} = AnkiConnect.find_notes(%{query: "deck:M-224"})
 
     sheet_ids =
       sheet_rows
@@ -31,40 +52,16 @@ defmodule Fosfosol do
       |> Enum.reverse()
 
     sheet_needs_id = anki_ids -- sheet_ids
-    # anki_needs_cards = Enum.reject(sheet_rows, fn
-    #   # [row_number, front, back, id]
-    #   # if there is no ID, the length of the row is 3
-    #   row -> length(row) == 4
-    # end)
+
+    # Once we know the IDs present on Anki but not Sheets, we load their
+    # content from Anki as a source of truth.
     {:ok, notes} = AnkiConnect.notes_info(%{notes: sheet_needs_id})
     proper_notes = Enum.map(notes, &properize_note/1)
 
+    # Equipped with that data, we figure out whether it is just the ID that's
+    # missing from the sheet, or the whole word, or if something's amiss.
     [notes_with_row_numbers, missing_from_spreadsheet, errors] =
-      Enum.map(proper_notes, fn
-        %{front: note_front, back: note_back, id: note_id} ->
-          # search db rows to include the corresponding row ID in these maps
-          front_side_row = Enum.find_value(sheet_rows, finder(1, note_front))
-          back_side_row = Enum.find_value(sheet_rows, finder(2, note_back))
-          comparison = compare(front_side_row, back_side_row)
-
-          case comparison do
-            :missing ->
-              {:missing, [note_front, note_back, note_id]}
-
-            :error ->
-              {:error,
-               "Anki note \"#{note_front}\" (#{front_side_row})/\"#{note_back}\" (#{back_side_row})found different matches in the spreadsheet."}
-
-            :front ->
-              {:ok, [front_side_row, note_front, note_back, note_id]}
-
-            :back ->
-              {:ok, [back_side_row, note_front, note_back, note_id]}
-
-            _ ->
-              {:error, "Unknown error"}
-          end
-      end)
+      Enum.map(proper_notes, workhorse(sheet_rows))
       |> Enum.reduce([[], [], []], fn
         {:ok, value}, [hits, missing, errors] -> [[value | hits], missing, errors]
         {:missing, value}, [hits, missing, errors] -> [hits, [value | missing], errors]
@@ -72,10 +69,114 @@ defmodule Fosfosol do
       end)
       |> Enum.map(&Enum.reverse/1)
 
-    # {_rows, } = missing_from_spreadsheet
-    # # |> Enum.reduce
-    # |> IO.inspect()
-    IO.inspect(notes_with_row_numbers)
+    notes_with_row_numbers =
+      notes_with_row_numbers
+      |> Enum.sort(fn [row1 | _rest1], [row2 | _rest2] -> row1 < row2 end)
+
+    write.("Notes with row numbers:")
+    write.(notes_with_row_numbers)
+    write.("Notes missing from the spreadsheet:")
+    write.(missing_from_spreadsheet)
+    write.("Errors:")
+    write.(errors)
+    :ok = File.close(file)
+
+    if length(notes_with_row_numbers) > 0 or length(missing_from_spreadsheet) > 0 do
+      write.("Inserting and updating into the sheet")
+
+      insert_notes_into_sheet(
+        sheet,
+        notes_with_row_numbers,
+        missing_from_spreadsheet,
+        hd(List.last(sheet_rows))
+      )
+      |> write.()
+    end
+
+    if length(anki_needs_cards) > 0, do: create_flashcards_and_update_ids(sheet, anki_needs_cards)
+  end
+
+  defp insert_notes_into_sheet(sheet, updates, inserts, last_row) do
+    ranges = Enum.map(updates, fn [row | _data] -> "A#{row}:C#{row}" end)
+    values = Enum.map(updates, &tl/1)
+
+    update_data =
+      case Sheet.write_rows(sheet, ranges, values) do
+        {:ok, data} ->
+          IO.puts("Should have written #{length(ranges)} IDs to spreadsheet.")
+          data
+
+        {:error, exception} ->
+          Exception.format(:error, exception)
+      end
+
+    insert_data =
+      case Sheet.append_rows(sheet, last_row + 1, inserts) do
+        {:error, exception} ->
+          Exception.format(:error, exception)
+
+        data ->
+          IO.puts("Should have appent #{inserts.length} rows to spreadsheet.")
+          data
+      end
+
+    {update_data, insert_data}
+  end
+
+  defp create_flashcards_and_update_ids(sheet, rows) do
+    notes = Enum.map(rows, &build_flashcard/1)
+    {:ok, new_flashcard_ids} = AnkiConnect.add_notes(%{notes: notes})
+
+    updates =
+      rows
+      |> Enum.zip(new_flashcard_ids)
+      |> Enum.map(fn {row, id} -> row ++ [id] end)
+
+    insert_notes_into_sheet(sheet, updates, [], 320)
+  end
+
+  defp build_flashcard([_row, front, back]) do
+    base =
+      "./config/settings.json"
+      |> File.read!()
+      |> Jason.decode!(keys: :atoms)
+      |> Map.drop(~w[front back file_id last_updated]a)
+
+    Map.put(base, :fields, %{Front: front, Back: back})
+  end
+
+  defp workhorse(sheet_rows) do
+    fn [note_front, note_back, note_id] ->
+      # search db rows to include the corresponding row ID in these maps
+      front_side_row = Enum.find_value(sheet_rows, finder(1, note_front))
+      back_side_row = Enum.find_value(sheet_rows, finder(2, note_back))
+      comparison = compare(front_side_row, back_side_row)
+
+      case comparison do
+        :missing ->
+          {:missing, [note_front, note_back, note_id]}
+
+        :error ->
+          {:error,
+           "Anki note \"#{note_front}\" (#{front_side_row})/\"#{note_back}\" (#{back_side_row})found different matches in the spreadsheet."}
+
+        :front ->
+          {:ok, [front_side_row, note_front, note_back, note_id]}
+
+        :back ->
+          {:ok, [back_side_row, note_front, note_back, note_id]}
+
+        _ ->
+          {:error, "Unknown error"}
+      end
+    end
+  end
+
+  defp build_writer(file) do
+    fn contents ->
+      options = [limit: :infinity, printable_limit: :infinity]
+      IO.inspect(file, contents, options)
+    end
   end
 
   defp deflag(binary) do
@@ -84,11 +185,11 @@ defmodule Fosfosol do
   end
 
   defp properize_note(note) do
-    %{
-      id: note["noteId"],
-      front: deflag(note["fields"]["Front"]["value"]),
-      back: deflag(note["fields"]["Back"]["value"])
-    }
+    [
+      deflag(note["fields"]["Front"]["value"]),
+      deflag(note["fields"]["Back"]["value"]),
+      note["noteId"]
+    ]
   end
 
   defp compare(front, back) do
@@ -104,7 +205,7 @@ defmodule Fosfosol do
     fn row -> if Enum.at(row, position) == needle, do: hd(row) end
   end
 
-  defp read_sheet_ids(sheet) do
+  defp read_sheet_rows(sheet) do
     {:ok, row_count} = Sheet.rows(sheet)
 
     2..row_count
